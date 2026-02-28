fd07284b3ddfc9126123a3f7e0da6757a5e38d76
Janisha Jyoti <janishajyoti@gmail.com>
2026-02-28 20:24:49 +0530

    Fix MongoDB Atlas connection-4

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from "mongodb";
 
 const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  tls: true,
+  serverSelectionTimeoutMS: 5000
+});
 console.log("DB file loaded");
 console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
 let isConnected = false;
